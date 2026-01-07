11e67151fbd4c8de07ce6756cc7767610225fd2f
Ben Szymanski <bszyman@icloud.com>
2026-01-07 02:26:12 -0600

    Update config to point to release 1.0.0 binary

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HyperGooey",
-            url: "",
-            checksum: ""
+            url: "https://github.com/bszyman/hypergooey-mac-dist/releases/download/1.0.0/HyperGooey.xcframework.zip",
+            checksum: "ba6e9ffc89eb119dd3dab625730acf73d826bcc59cc23502d89e4166ffc021d9"
         )
     ]
 )
