e1559265f553658d1bbdf9f5b4a89731b6b69366
Ben Szymanski <bszyman@icloud.com>
2026-03-26 15:58:13 -0500

    Fixes and upgraded appearance when rendering content on MacOS

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HyperGooey",
-            url: "https://github.com/bszyman/hypergooey-mac-dist/releases/download/1.1.0/HyperGooey.xcframework.zip",
-            checksum: "4a71fc1fe18e46a42f539a50f8a3e26816dcb8f61d754bc99f0754aa05f7787a"
+            url: "https://github.com/bszyman/hypergooey-mac-dist/releases/download/1.2.0/HyperGooey.xcframework.zip",
+            checksum: "b41494d93e4be4a9b4e6acf645f9dbe1023dd2a64c81ceef55ccdd667cae0d5c"
         )
     ]
 )
